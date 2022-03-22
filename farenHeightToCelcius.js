3ec1ff636f068b7db4ea270bd84c1b9d83ea625c
Faysal Mahmud <faysalmahmud735@gmail.com>
2022-03-22 23:35:08 +0600

    farenheit to celcius

--- farenHeightToCelcius.js
+++ farenHeightToCelcius.js
@@ -1,1 +1,12 @@
 // ২. একইভাবে উল্টা হিসাব করবে। যাতে তোমাকে ফারেনহাইট হিসেবে তাপমাত্রা দিলে সেটাকে সেলসিয়াস এ কনভার্ট করে আউটপুট দিবে। 
+
+function FarenToCel(faren) {
+    return ((5*faren)-32)/9;
+}
+
+var faren = 86;
+
+console.log("Temperature in Farenheit: "+faren+ "F");
+var cel = FarenToCel(faren);
+console.log("Temperature in Celcius: "+cel+ "C");
+
